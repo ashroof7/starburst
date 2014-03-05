fbbb3278ec44b452464a8841b49d81e78ba62091
Motaz <motaz@motaz-Inspiron-5521.(none)>
2014-03-05 03:11:02 +0200

    adding CenterOfFocus Class

---
+++ src/CenterOfFocus.h
@@ -0,0 +1,31 @@
+/*
+ * CenterOfFocus.h
+ *
+ *  Created on: Mar 5, 2014
+ *      Author: motaz
+ */
+
+#ifndef CENTEROFFOCUS_H_
+#define CENTEROFFOCUS_H_
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#define w 1000
+
+using namespace cv;
+using namespace std;
+
+class CenterOfFocus {
+
+public:
+	int state;
+	CenterOfFocus();
+	void drawCircle(Mat img, Point center);
+	int getCOF();
+	virtual ~CenterOfFocus();
+};
+
+#endif /* CENTEROFFOCUS_H_ */
